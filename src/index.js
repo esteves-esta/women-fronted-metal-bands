097817741c99bf47ed1086addbb871dcd0353585
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2023-11-24 14:53:17 -0300

    feat: base styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import './reset.css';
+import './styles.css';
 
 const container = document.getElementById("app");
 const root = createRoot(container)
