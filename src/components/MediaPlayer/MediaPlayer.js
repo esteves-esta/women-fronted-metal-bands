1a24efc19b959df46b072bcf1bb474426b44a55b
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2023-12-06 10:32:51 -0300

    style: changing media player styles and some colors

--- src/components/MediaPlayer/MediaPlayer.js
+++ src/components/MediaPlayer/MediaPlayer.js
@@ -1,7 +1,7 @@
 //credits: JOSH COMEU - JOY OF REACT
 {/* https://dev.to/lukewduncan/how-to-build-an-audio-player-with-html5-and-the-progress-element-387m */ }
 import React from "react";
-import { Play, Pause, Volume1, Volume2, VolumeX, AudioLines } from "lucide-react";
+import { PlayCircle, PauseCircle, Volume1, Volume2, VolumeX, AudioLines } from "lucide-react";
 import * as classes from './MediaPlayer.module.css';
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import { DeezerContext } from '../DeezerProvider';
@@ -95,8 +95,11 @@ function MediaPlayer() {
   return (
     <div className={classes.mediaPlayer} style={{ display: src ? 'block' : 'none' }}>
       <div className={classes.info}>
-        <AudioLines />
-        <p className="label">Playing preview via deezer api</p>
+        <div className="flex flex-row gap-4 items-center">
+          <AudioLines />
+          <p className="label mb-0">Playing preview via deezer api</p>
+
+        </div>
       </div>
 
       <div className={classes.mediaPlayerBody} >
@@ -108,7 +111,7 @@ function MediaPlayer() {
           />
             : <LoaderSvg />}
           <div className={classes.summary}>
-            <p className="label">{trackIsLoading ? 'loading' : title}</p>
+            <p className={classes.title}>{trackIsLoading ? 'loading' : title}</p>
             {!trackIsLoading && <small>{artist}</small>}
           </div>
         </div>
@@ -123,11 +126,16 @@ function MediaPlayer() {
               setIsPlaying(!isPlaying);
             }}
           >
-            {isPlaying ? <Pause /> : <Play />}
+            {isPlaying ? <PauseCircle /> : <PlayCircle />}
             <VisuallyHidden.Root>Toggle playing</VisuallyHidden.Root>
           </button>
 
-          {!!audioRef.current?.duration && <p className="mb-0">00:{currentTimeFormatted} / 00:{Math.round(audioRef.current.duration)}</p>}
+          {!!audioRef.current?.duration &&
+            (<p className="mb-0 font-bold">
+              00:{currentTimeFormatted} 
+              {/* / 00:{Math.round(audioRef.current.duration)} */}
+            </p>)
+          }
 
           <audio
             onTimeUpdate={updateProgressBar}
@@ -139,21 +147,21 @@ function MediaPlayer() {
             }}
           />
           <progress ref={progressBarRef} id="seek-obj" value={progressValue} max="1" />
-
-          <button
-            onKeyDown={(event) => {
-              if (event.code === "Space") {
-                event.stopPropagation();
-              }
-            }}
-            onClick={changeVolume}
-          >
-            {volume === 1 && <Volume1 />}
-            {volume === 2 && <Volume2 />}
-            {volume === 0 && <VolumeX />}
-            <VisuallyHidden.Root>Change volume</VisuallyHidden.Root>
-          </button>
         </div>
+        <button
+          onKeyDown={(event) => {
+            if (event.code === "Space") {
+              event.stopPropagation();
+            }
+          }}
+          onClick={changeVolume}
+        >
+          {volume === 1 && <Volume1 />}
+          {volume === 2 && <Volume2 />}
+          {volume === 0 && <VolumeX />}
+          <VisuallyHidden.Root>Change volume</VisuallyHidden.Root>
+        </button>
+
 
       </div>
     </div >
