28cb9045ed8fd1ae22a3b310e581f8ea104ca481
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2023-12-08 21:16:40 -0300

    all bands found on text file - separated file off name to check info still

--- list-of-metal-bands/band names - to gather info.ts
+++ list-of-metal-bands/band names - to gather info.ts
@@ -1,56 +1,387 @@
 //http://www.listdiff.com/compare-2-lists-difference-tool
-["Alain Johanne",
-"Apocalyptica",
-"Bala",
-"Blackbriar",
-"Brutus",
-"Capra",
-"Charlotte Wessels",
-"Dogma",
-"Doro",
-"Eleine",
-"Folterkammer",
-"Furies",
-"Girlschool",
-"HANABIE.",
-"Heriot",
-"Inglorious",
-"Isyana Sarasvati",
-"Lilith Czar",
-"Lutharo",
-"Metalite",
-"Mimi Barks",
-"Motive Black",
-"Myrkur",
-"New Years Day",
-"Predatory Void",
-"Saint Agnes",
-"Skarlett Riot",
-"Sophie Lloyd",
-"Svalbard",
-"Tarja Turunen",
-"The Pretty Reckless",
-"Voice of Baceprot",
-"Wolvennest",
-"chelsea wolfe",
-"WICKED ASYLUM",
-"Shewolf",
-"CASSIDY PARIS",
-"ACCIDENTAL PRESIDENT",
-"SANGHAM",
-"PROJECT RENEGADE",
-"MNEMOS",
-"VALAR MORGHULIS",
-"FAYE WEBSTER",
-"CONSPIRACY OF BLACKNESS",
-"NEVER OBEY AGAIN",
-"PANZERCHRIST",
-"NOVA DOLL",
-"EXILED HOPE",
-"SINHERESY",
-"THE FIXER",
-"HOLY SHIRE",
-"venus 5",
-"CHRYSARMONIA",
-"ILLUMISHADE",
-"HER ALONE"]
+/* -- NEW NEW
+Alain Johanne
+Apocalyptica
+Bala
+Blackbriar
+Brutus
+Capra
+Charlotte Wessels
+Dogma
+Doro
+Eleine
+Folterkammer
+Furies
+Girlschool
+HANABIE.
+Heriot
+Inglorious
+Isyana Sarasvati
+Lilith Czar
+Lutharo
+Metalite
+Mimi Barks
+Motive Black
+Myrkur
+New Years Day
+Predatory Void
+Saint Agnes
+Skarlett Riot
+Sophie Lloyd
+Svalbard
+Tarja Turunen
+The Pretty Reckless
+Voice of Baceprot
+Wolvennest
+chelsea wolfe
+WICKED ASYLUM
+Shewolf
+CASSIDY PARIS
+ACCIDENTAL PRESIDENT
+SANGHAM
+PROJECT RENEGADE
+MNEMOS
+VALAR MORGHULIS
+FAYE WEBSTER
+CONSPIRACY OF BLACKNESS
+NEVER OBEY AGAIN
+PANZERCHRIST
+NOVA DOLL
+EXILED HOPE
+SINHERESY
+THE FIXER
+HOLY SHIRE
+venus 5
+CHRYSARMONIA
+ILLUMISHADE
+HER ALONE
+veil of secrets
+asylum pyre
+aevum
+aura
+blóð
+dark sarah
+shield of wings
+sojourner
+walk in darkness
+dama
+tristania
+and then she came
+аркона [arkona]
+asrai
+coma divine
+unsun
+dark horse white horse
+arcane existence
+whyzdom
+celtian
+aetherea
+venice may
+soulspell
+the birthday massacre
+tulip
+snow white blood
+vuur
+alight
+vodun
+ardours
+theatre of tragedy
+almôra
+trail of tears
+vetrar draugurinn
+astralium
+crusade of bards
+the answer lies in the black void
+star eternal
+sins in vain
+bare infinity
+tarja
+chaos magic
+trophallaxy
+arsenic addiction
+unlucky morpheus
+catalyst crime
+visions of atlantis
+cellar darling
+beneath my sins
+smackbound
+aperion
+the dark element
+антиллия [antillia]
+rage of light
+valcata
+as night falls
+crimson sun
+tystnaden
+battlelore
+deadlock
+celestivl
+degrees of truth
+beautiful sin
+coldbound
+akoma
+beseech
+against myself
+silverstage
+therion
+chaostar
+the sins of thy beloved
+synthwailer
+diabulus in musica
+voices of destiny
+this is oblivion
+murmurs
+ethereal sin
+vaee solis
+nebrus
+xolotl
+xxgekixx
+13 winters
+arsenik
+mysterious eclipse
+dies ater
+mystherium
+black dementia
+atenoux
+menace ruine
+anapilis
+oathean
+a winter lost
+exhumer
+frost despair
+wykked wytch
+mons lvnae
+zion
+mortt
+dask
+manslaughter
+bestial holocaust
+gloomy grim
+afterlife
+elegeion
+morak
+trinacria
+somnus
+throne of malediction
+tengereken
+unholy throne
+einsicht
+atrum inritus
+occultist
+the archivist
+mortals
+nocra
+my fatality
+megaera
+nàttsòl
+ethereal
+dödsängel
+endlessnight
+mordichrist
+twilight is mine
+adultery
+tunes of despair
+forever slave
+ebonylake
+miasthenia
+funeral countess
+nashmeh
+utburd
+the antiprism
+t h a c l t h i
+zenith maudlin
+beaten victoriouses
+ephel duath
+ad inferna
+godemiché
+faethom
+evil lucifera
+macbeth
+malnàtt
+dreariness
+eschatos
+mylidian
+bitter reflections
+martyrium
+avathar
+occult
+twin obscenity
+treachery
+uranus
+north diamond
+ol sonuf
+morigan
+ancient
+nargathrond
+equimanthorn
+voice of midnight
+furva ambiguitas
+wavelength:satan
+nachtlieder
+darzamat
+musspellheim
+theatres des vampires
+devil may cry
+amethista
+apostasy
+wartanas
+straightout
+spiritus tenebrae
+demonic christ
+fangorn
+foreverdark woods
+aversion
+deborah
+fullmoon's insignia
+asaru
+dominion iii
+dead
+møllehøj
+on
+evol
+amesoeurs
+sortilegia
+necrosadistic goat torture
+alkonost
+dreamlike horror
+winterthrall
+acheron
+black reign [us]
+death dies
+arcane grail
+gate to khaos
+malsain
+adorior
+war ancestry
+xantotol
+mother of the hydra
+anguished
+angryon
+ancestral legacy
+a cloud forest
+messershmit
+drastique
+strynn
+edenkaiser
+draconic
+meza virs
+tears of ea
+v-kaos
+alkahest
+soulgrind
+benatnash
+awake the mighty dragon
+acherontas
+angel of anger
+dismal euphony
+ancestral volkhves
+vomitwolves
+ahna
+vindicta
+negatron
+false
+antichrisis
+endlesshade
+aherusia
+witchmark
+ashtar
+depressive reality
+malevolentia
+gedanken toten lebens
+ludicra
+sorrowseed
+zmrok
+uhl
+alocer
+folivore
+belligerence
+witchblood
+feikn
+fornace
+will of hatred
+nepharitus
+maledictvs
+artep
+.nema
+askeregn
+nornír
+natan
+streben
+н.о.в.а.р.а. [n.o.v.a.r.a.]
+esfinge de gizeh
+final
+valfreya
+axidance
+niflheim
+darkspace
+at odds with god
+nocturnal worshipper
+oizys
+ampulheta
+bagronk
+act of god
+ocularis infernum
+malzera
+dei tetra
+witch cult [us]
+tenebra
+urticant
+nyx
+demoniarch
+lullaby
+unholy goatfucker
+addaura
+absidia
+devine essence
+ancient ceremony
+annika eronen
+andry lagiou
+alona levina
+angelwings
+arthemisia
+angelina vahera
+ayracsana
+ann my guard
+aurora-project
+amoth
+apolinara
+amy lee
+auraleak
+ava toton
+arshenic
+17 crash
+arthemion
+anneke van giersbergen
+atwood
+avril lavigne
+avienne
+alyona "ultraviolet"
+ashley bean
+anna manolaraki
+archetype x
+angela busato
+annie clark
+ap tobler
+anna kiraly
+arch enemy 2
+alissa white-gluz 2
+after evolution
+attractive chaos
+asymmetry
+angela gossow
+autumn's grief
+aura danciulescu
+astray valley
+annette ignis
+angel nation
+am samstag
+anette olzon
+angela di vincenzo
+andrea carrero
+april art
+anna golebiowska
+arcana opera
+anna olchawa
+avelina de moray
+aurelie martins
+*/
