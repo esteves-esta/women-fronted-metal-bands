5e08b93a946587ea40eb92cff95ef29a090f263f
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2024-09-15 01:02:21 -0300

    refactor: toogle group - to styled components

--- app/src/components/ToogleGroupButton/ToogleGroupButton.tsx
+++ app/src/components/ToogleGroupButton/ToogleGroupButton.tsx
@@ -1,28 +1,59 @@
-import classes from './ToogleGroupButton.module.css';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import { LucideIcon } from 'lucide-react';
+import { styled } from "styled-components";
 
 function ToogleGroupButton({ list, currentValue, onChange }: {
   list: { value: any, text: string, icon?: LucideIcon, iconOnly?: boolean }[],
   currentValue: any,
   onChange: (val) => void
 }) {
-  return <ToggleGroup.Root className={classes.group} type="single" defaultValue={currentValue} onValueChange={onChange}>
+  return <Group type="single" defaultValue={currentValue} onValueChange={onChange}>
     {list.map(({ text, value, icon, iconOnly }) => {
       const Icon = icon
       return (
-        <ToggleGroup.Item
+        <GroupItem
           key={value}
-          className={currentValue === value.toString() ? `${classes.groupItemActive} ${classes.groupItem}` : classes.groupItem}
+          className={currentValue === value.toString() ? 'active' : ''}
           value={value.toString()}
           aria-label={text}
         >
           {!iconOnly && text}
           {!!icon && <Icon size={18} />}
-        </ToggleGroup.Item>
+        </GroupItem>
       )
     })}
-  </ToggleGroup.Root>;
+  </Group>;
 }
+
+const GroupItem = styled(ToggleGroup.Item)`
+border: none;
+border-radius: 4rem;
+background-color: var(--color-secondary);
+color: var(--text-color);
+padding: 4px 16px;
+font-weight: bold;
+letter-spacing: .05rem;
+cursor: pointer;
+display: flex;
+gap: 10px;
+&:focus {
+  box-shadow: var(--box-shadow-focus);
+}
+
+&.active {
+  background: var(--color-primary-dark);
+}
+`;
+
+const Group = styled(ToggleGroup.Root)`
+gap: 1em;
+display: flex;
+flex-wrap: wrap;
+
+@media ${(p) => p.theme.queries.tabletAndUp} {
+}
+`;
+
+
 
 export default ToogleGroupButton;
