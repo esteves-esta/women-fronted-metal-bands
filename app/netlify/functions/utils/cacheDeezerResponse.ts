95cfda31b43b3dcb8ffac01a491e39a0a04e17d4
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2025-06-03 21:30:03 -0300

    fix: remove caching from deezerResponse api

--- app/netlify/functions/utils/cacheDeezerResponse.ts
+++ app/netlify/functions/utils/cacheDeezerResponse.ts
@@ -1,4 +1,3 @@
-import { connectCacheClient } from "./cacheDatabase";
 import { connectClient, formatYearsActive } from "./database";
 
 export async function cacheResponses(
@@ -14,32 +13,6 @@ export async function cacheResponses(
       response: null,
     };
   }
-  let clientUpstash;
-
-  let cacheExist;
-
-  try {
-    clientUpstash = await connectCacheClient();
-    // cacheExist = await client.EXISTS(`artist:${artistId}`);
-    cacheExist = await clientUpstash.json.get(`${endpoint}${endpoint2}:${id}`);
-  } catch (e) {
-    console.log("cliente " + e);
-
-    return {
-      error: false,
-      status: 500,
-      message: "Connection error.",
-      response: null,
-    };
-  }
-
-  if (cacheExist !== null)
-    return {
-      error: false,
-      status: 200,
-      message: null,
-      response: cacheExist,
-    };
 
   let responseResult;
   try {
@@ -56,19 +29,28 @@ export async function cacheResponses(
       }
     );
 
-    if (response.status === 200) {
-      responseResult = await response.json();
-    }
+
     if (
       response.status !== 200 ||
       (responseResult.error && responseResult.error?.code === 800)
-    )
+    ) {
       return {
         error: true,
         status: response.status,
         message: null,
         response: response,
       };
+    }
+
+    if (response.status === 200) { responseResult = await response.json(); }
+
+    return {
+      error: false,
+      status: 200,
+      message: null,
+      response: responseResult,
+    };
+
   } catch (e) {
     // console.log({ json: e });
     return {
@@ -78,49 +60,4 @@ export async function cacheResponses(
       response: e,
     };
   }
-
-  const DEEZER_EMPTY_PICTURE =
-    "https://e-cdns-images.dzcdn.net/images/artist//500x500-000000-80-0-0.jpg";
-
-  const listJSON = require("../../../list-of-metal-bands/list.json");
-
-  try {
-    let clientBandDB;
-    if (cacheExist == null) {
-      await clientUpstash.json.set(
-        `${endpoint}${endpoint2}:${id}`,
-        "$",
-        responseResult
-      );
-
-      if (endpoint === "artist" && endpoint2 === "null") {
-        const itemFound = listJSON.find((item) => item.deezerId === Number(id));
-
-        if (itemFound) {
-          clientBandDB = await connectClient();
-          if (responseResult.picture_big === DEEZER_EMPTY_PICTURE)
-            itemFound.emptyPicture = true;
-
-          itemFound.deezerPicture = responseResult.picture_big;
-          await clientBandDB.json.set(`band:${id}`, "$", {
-            ...itemFound,
-            activeFor: formatYearsActive(itemFound),
-            numberOfVocalists: itemFound.currentVocalists.length,
-          });
-        }
-        // {deezerTrackInfo;}
-      }
-    }
-    await clientUpstash.quit();
-    await clientBandDB.quit();
-  } catch (e) {
-    console.error(`cache error: ${e}`);
-  }
-
-  return {
-    error: false,
-    status: 200,
-    message: null,
-    response: responseResult,
-  };
 }
